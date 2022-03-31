aedec9c366e87bd50a3ebb3d236c395c1c9a3d74
Exequiel65 <marcos97britos@gmail.com>
2022-03-31 11:36:04 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const logger = require('morgan');
 const session = require('express-session')
 const isSession = require('./middlewares/isSession');
+const cors = require('cors')
 // ************ express() - (don't touch) ************
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors())
 app.use(session({
     secret: "authSecret",
     resave: false,
